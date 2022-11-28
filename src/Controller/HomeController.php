ec15df805a883c74d3f00ba0f0345f99bd21e0b3
Nigialro <112541578+Nigialro@users.noreply.github.com>
2022-11-28 23:34:26 +0100

    Débuts sur Symfony

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,4 +13,34 @@ class HomeController extends AbstractController
     {
         return $this->render('home.html.twig');
     }
+
+    #[Route('/nobuo_uematsu', 'nobuo_uematsu', methods: ['GET'])]
+    public function uematsu():Response
+    {
+        return $this->render('composers/uematsu.html.twig');
+    }
+
+    #[Route('/gustavo_santaolalla', 'gustavo_santaolalla', methods: ['GET'])]
+    public function santaolalla():Response
+    {
+        return $this->render('composers/santaolalla.html.twig');
+    }
+
+    #[Route('/amin_bhatia', 'amin_bhatia', methods: ['GET'])]
+    public function bhatia():Response
+    {
+        return $this->render('composers/bhatia.html.twig');
+    }
+
+    #[Route('/joe_hisaishi', 'joe_hisaishi', methods: ['GET'])]
+    public function hisaishi():Response
+    {
+        return $this->render('composers/hisaishi.html.twig');
+    }
+
+    #[Route('/ari_posner', 'ari_posner', methods: ['GET'])]
+    public function posner():Response
+    {
+        return $this->render('composers/posner.html.twig');
+    }
 }
